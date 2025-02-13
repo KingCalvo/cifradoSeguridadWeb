51bdcac13673e4620334b1b6981c31af64bbc4a1
Enrique Calvo Garcia <enriquecalvo75@gmail.com>
2025-02-13 08:41:54 -0600

    Estructura arreglada y nav

--- cifrado/src/app/components/Nav.tsx
+++ cifrado/src/app/components/Nav.tsx
@@ -6,16 +6,16 @@ const Nav: React.FC = () => {
     <nav className="fixed top-3 w-full h-16 flex justify-center items-center z-50">
       <ul className="max-w-[700px] h-16 p-8 border-white border-2 bg-slate-600 flex justify-center items-center gap-8 rounded-3xl">
         <li className="hover:text-white hover:text-xl transition-all transform">
-          <Link href="/acci">Acci</Link>
+          <Link href="/acci">ASCII</Link>
         </li>
         <li className="hover:text-white hover:text-xl transition-all transform">
           <Link href="/cifrar">Cifrar</Link>
         </li>
         <li className="hover:text-white hover:text-xl transition-all transform">
-          <Link href="/hexadecimal">Desifrar</Link>
+          <Link href="/descifrar">Descifrar</Link>
         </li>
         <li className="hover:text-white hover:text-xl transition-all transform">
-          <Link href="/octal">Hash</Link>
+          <Link href="/hash">Hash</Link>
         </li>
       </ul>
     </nav>
---
+++ cifrado/src/app/hash/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AsciiHexTable from "../components/AsciiHexTable";
+
+const Page: React.FC = () => {
+  return (
+    <div className="mt-40">
+      <AsciiHexTable />
+    </div>
+  );
+};
+
+export default Page;
